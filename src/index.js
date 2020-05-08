697e8efd85202822a2f0c2fddab8e50ef5149f2d
Victor Hugo <57298432+victorugob@users.noreply.github.com>
2020-05-08 15:02:29 -0300

    atualização card

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import Carts from './Carts';
 
 ReactDOM.render(
   <>
   <App />
-  <Carts />
   </>,
   document.getElementById('root')
 );
